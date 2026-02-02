703e1fede566049f2bf94e81ee48eface47bdeee
ericwoe <eric1304@gmx.de>
2026-02-02 10:20:37 +0100

    land_mask erstellen in main.py fertig

--- main.py
+++ main.py
@@ -1,8 +1,14 @@
 import src.preprocessing
 import src.snow_model
+import src.land_mask
+import xarray as xr
+from pathlib import Path
+
 
 DATA_DIR = "./data/Model_Output_From_Harrison/Temp_Precip/nw_ur_150_07"
 PATTERN = "nw_ur_150_07.cam.h0.*.nc"
+GADM_FILE_PATH = "./data/GADM/gadm_410.gpkg"
+LAND_MASK_OUTPUT_PATH = "./data/interim/land_mask.nc"
 
 # Preprare Dataset
 ds = src.preprocessing.run_preprocessing(
@@ -12,4 +18,17 @@ ds = src.preprocessing.run_preprocessing(
 # Run Snow Model
 ds = src.snow_model.add_snow_variables(ds)
 
-print(ds.snow_storage)
+
+land_mask_path = Path(LAND_MASK_OUTPUT_PATH)
+# Create or import land mask
+if land_mask_path.exists():
+    print("Loading existing land mask")
+    mask = xr.open_dataarray(land_mask_path)
+else:
+    print("Creating land mask")
+    mask = src.land_mask.create_land_mask(ds, gadm_path=GADM_FILE_PATH)
+    print(f"Saving Mask to {LAND_MASK_OUTPUT_PATH}")
+    # Ensure directory exists
+    land_mask_path.parent.mkdir(parents=True, exist_ok=True)
+    # Save mask to NetCDF
+    mask.to_netcdf(land_mask_path)
